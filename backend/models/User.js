02dca18f51fb58201403d144f5b75bc3b2e444a5
Arbaz <arbaz582021@gmail.com>
2024-09-04 08:29:53 -0400

    add

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto-js')
 
 const UserSchema = new mongoose.Schema({
 
@@ -45,16 +46,21 @@ const UserSchema = new mongoose.Schema({
     required: function() { return this.userRole === 'houseOwner'; }, // Required only for houseOwners
     default: undefined //keep empty is not provided
   },
+  //reset password
+  resetPasswordToken: String,          // Token to be used for password reset
+  resetPasswordExpire: Date,           // Expiration time for the token
+
 })
 
 UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return // Only hash if password field is modified
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
-    { userId: this._id, name: this.name },
+    { userId: this._id, name: this.userName },
     process.env.JWT_SECRET,
     {
       expiresIn: process.env.JWT_LIFETIME,
@@ -66,5 +72,20 @@ UserSchema.methods.comparePassword = async function (canditatePassword) {
   const isMatch = await bcrypt.compare(canditatePassword, this.password)
   return isMatch
 }
+
+// Generate and hash password reset token
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex')
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+  // Set expire time (e.g., 10 minutes)
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000
+
+  return resetToken
+}
+
+
 
 module.exports = mongoose.model('User', UserSchema)
