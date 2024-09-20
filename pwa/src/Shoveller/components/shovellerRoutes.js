4ab793c236e1bdee3e29a7edd64a5cc48403cb14
Arbaz <arbaz582021@gmail.com>
2024-09-20 10:00:34 -0400

    add

--- pwa/src/Shoveller/components/shovellerRoutes.js
+++ pwa/src/Shoveller/components/shovellerRoutes.js
@@ -9,12 +9,14 @@ import ServiceProgressShoveller from "./serviceProgress";
 import ServiceFinishedByShoveller from "./serviceFinished";
 import ShovellerPersonalDetail from "./shovellerPersonalInfo";
 import ShovellerSignupProcess from "./shovellerSignupProcess";
+import StripeOnboard from "./stripeOnboarding";
 
 const route = [
     // { path:'/shovellerDetails', element: <ShovellerDetails /> },
     // { path:'/serviceDetail', element: <ServiceDetail /> },
     // { path:'/shovellerPersonalDetail', element: <ShovellerPersonalDetail /> },
     { path:'/shovellerSignupProcess', element: <ShovellerSignupProcess /> },
+    { path:'/stripeOnboard', element: <StripeOnboard /> },
     { path:'/paymentSingIn', element: <PayementSingIn /> },
     { path:'/paymentSingUp', element: <PaymentSingUp /> },
     { path:'/searchJobByMap', element: <SeatchJobByMap /> },
